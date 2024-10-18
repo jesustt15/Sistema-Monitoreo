dc237423a73c92e2b3ec8a789a526adf1ae84bc7
jesustt15 <jesustoussaint08@gmail.com>
2024-10-18 15:45:48 -0400

    se empieza el navbar
    pipuuu

--- front-temp/src/components/NavBar.jsx
+++ front-temp/src/components/NavBar.jsx
@@ -1,12 +1,19 @@
-import { Link, NavLink } from 'react-router-dom';
-import './components.css';
+import { NavLink} from 'react-router-dom';
+import './components.scss';
 import { useAuth } from '../context/AuthContext';
+import { useState } from 'react';
 
 
 
 const Navbar = () => {
 
   const {logout} = useAuth();
+  const {active, setActive } = useState('');
+
+  const handleClick = (name) =>{
+    setActive(name);
+
+  }
 
   const handleLogout = () =>{
     logout();
@@ -14,11 +21,24 @@ const Navbar = () => {
   
   return (
     <nav className="navbar">
+      <div className="usuario">Jesus Toussaint</div>
       <div className="navbar-brand">
-        <Link to="/">Valores</Link>
-        <NavLink to="/config">Config</NavLink>
-        <NavLink to="/lugares">Localidades</NavLink>
-        <NavLink to="/historico">Historico</NavLink>
+        <NavLink
+        className={`nav-link ${active === 'valores' ? 'active' : ''}`}
+        onClick={ () => handleClick('valores')}
+        to="/">Valores</NavLink>
+        <NavLink 
+        className={`nav-link ${active === 'config' ? 'active' : ''}`}
+        onClick={ () => handleClick('config')}
+        to="/config">Config</NavLink>
+        <NavLink 
+        className={`nav-link ${active === 'lugares' ? 'active' : ''}`}
+        onClick={ () => handleClick('lugares')}
+        to="/lugares">Localidades</NavLink>
+        <NavLink 
+        className={`nav-link ${active === 'hist' ? 'active' : ''}`}
+        onClick={ () => handleClick('hist')}
+        to="/historico">Histórico</NavLink>
         <button onClick={handleLogout}>Salir</button>
       </div>
     </nav>
